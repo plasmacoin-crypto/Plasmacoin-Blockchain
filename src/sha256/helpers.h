8d4e279f24f418a77b3c798c7b7caccaf4209488
Ryan Smith <rysmith2113@gmail.com>
2021-01-24 14:10:14 -0500

    Remove unnecessary code

--- src/sha256/helpers.h
+++ src/sha256/helpers.h
@@ -8,20 +8,5 @@
 #ifndef SHA256_HELPERS_H
 #define SHA256_HELPERS_H
 
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-// Convert a decimal number (i.e., and integer) to its Big Endian
-// hexadecimal equivalent.
-uint32_t tohex(int dec) {
-	char* str[10000]; // Arbitrarily allocate 10000 characters
-	sprintf(*str, "%d", dec); // Convert the decimal to a string
-
-	for (int i = 0; str[i] != "\0"; i += 1) {
-
-	}
-}
-
 #endif // SHA256_HELPERS_H
 
