31a02bcde17c45a480411377bbf3823aa366fb57
Ryan Smith <rysmith2113@gmail.com>
2021-01-25 22:20:18 -0500

    Add a source file to test the code

---
+++ src/sha256/sha256.cpp
@@ -0,0 +1,18 @@
+//
+// FILENAME: sha256.cpp | Plasmacoin Cryptocurrency
+// DESCRIPTION: A source file to test the SHA-256 algorithm
+// CREATED: 2021-01-25 @ 9:03 PM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#include <bitset>
+#include <iostream>
+
+#include "helpers.hpp"
+
+int main() {
+	bitset<8> byte = std::bitset<8>(35);
+	std::cout << rotateRight(byte);
+
+	return 0;
+}
