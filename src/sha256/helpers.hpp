ca84298694a36aa1779e9f83f51471aa9d84e2b9
Ryan Smith <rysmith2113@gmail.com>
2021-01-29 21:56:48 -0500

    Fix some indentation issues

--- src/sha256/helpers.hpp
+++ src/sha256/helpers.hpp
@@ -49,13 +49,13 @@ bitset<8> shiftRight(bitset<8> byte, int n = 1) {
 
 // Concatenate two bytes
 int concat(int b1, int b2) {
-    return (b1 << 8) | b2;
+	return (b1 << 8) | b2;
 }
 
 // Concatenate two 8-bit (1 byte) bitsets and return the
 // result as a string
 string strconcat(bitset<8> b1, bitset<8> b2) {
-    uint64_t combined = (b1.to_ulong() << 8) | b2.to_ulong(); // Get the integer representation of the two bytes
+	uint64_t combined = (b1.to_ulong() << 8) | b2.to_ulong(); // Get the integer representation of the two bytes
 	return bitset<16>(combined).to_string();
 }
 
@@ -108,49 +108,54 @@ bitset<8> sigma_1(bitset<8> x) {
 //
 
 vector<bitset<32>> decompose(string text) {
-    string textcpy = text;
-    int i = 0;
-    vector<bitset<32>> blocks;
+	string textcpy = text;
+	int i = 0;
+	vector<bitset<32>> blocks;
 
-    while (i < 63) {
-        if (i < 16) { // Chunks 1-16
-            // Save a 32-bit chink of data to the vector
-            blocks.push_back(bitset<32>(text.substr(0, 32)));
-            textcpy.erase(0, 32); // Will erase indices 0-31 (Interval notation: [0, 32))
-        }
-        else { // Chunks 17-63
-            // Calculate the remaining chunks using the following
-            // formula:
-            //
-            // W_i = σ_1(W_(i − 2)) + W_(i − 7) + σ_0(W_(i − 15)) + W_(i − 16)
-            //
-            string blockstr = "";
+	while (i < 63) {
+		if (i < 16) { // Chunks 1-16
+			// Save a 32-bit chink of data to the vector
+			blocks.push_back(bitset<32>(text.substr(0, 32)));
+			textcpy.erase(0, 32); // Will erase indices 0-31 (Interval notation: [0, 32))
+		}
+		else { // Chunks 17-63
+			// Calculate the remaining chunks using the following
+			// formula:
+			//
+			// W_i = σ_1(W_(i − 2)) + W_(i − 7) + σ_0(W_(i − 15)) + W_(i − 16)
+			//
+			string blockstr = "";
 
-            // A place to store string versions of bytes from the 32-bit
-            // block that is currently being processed.
-            string inp1, inp2, inp3, inp4;
+			// A place to store string versions of bytes from the 32-bit
+			// block that is currently being processed.
+			string inp1, inp2, inp3, inp4;
 
-            // Process the 32 bits in chunks of 8 bits (1 byte) each
-            for (unsigned int j = 0; j < blocks[i].size(); j += 8) {
-                blockstr += (
-                    //
-                    // Each access of the vector will get the bytes from
-                    // j to j + 7 upper-bound exclusive (i.e., [j, j + 7) in
-                    // interval notation).
-                    //
-                    // 4 total bytes will be processed:
-                    // indices [0, 8), [8, 16), [16, 24), and [24, 32)
-                    //
-                    sigma_1(bitset<8>(blocks[i - 2].to_string().substr(j, j + 8))).to_string() + \
-                    blocks[i - 7].to_string().substr(j, j + 8) + \
-                    sigma_0(bitset<8>(blocks[i - 15].to_string().substr(j, j + 8))).to_string() + \
-                    blocks[i - 16].to_string().substr(j, j + 8)
-                );
-            }
-        }
-    }
+			// Process the 32 bits in chunks of 8 bits (1 byte) each
+			for (unsigned int j = 0; j < blocks[i].size(); j += 8) {
+				inp1 = sigma_1(bitset<8>(blocks[i - 2].to_string().substr(j, j + 8))).to_string();
+				inp2 = blocks[i - 7].to_string().substr(j, j + 8);
+				inp3 = sigma_0(bitset<8>(blocks[i - 15].to_string().substr(j, j + 8))).to_string();
+				inp4 = blocks[i - 16].to_string().substr(j, j + 8);
 
-    return blocks;
+				//
+				// Each access of the vector will get the bytes from
+				// j to j + 7 upper-bound exclusive (i.e., [j, j + 7) in
+				// interval notation).
+				//
+				// 4 total bytes will be processed:
+				// indices [0, 8), [8, 16), [16, 24), and [24, 32)
+				//
+				blockstr += (
+					 + \
+					 + \
+					 + \
+
+				);
+			}
+		}
+	}
+
+	return blocks;
 }
 
 //
@@ -159,14 +164,14 @@ vector<bitset<32>> decompose(string text) {
 
 // Convert a byte array represented as a vector if 8-bit bitsets to a string
 string stringify(vector<bitset<8>> bytearray) {
-    vector<bitset<8>>::iterator iter;
-    string bytestr = "";
+	vector<bitset<8>>::iterator iter;
+	string bytestr = "";
 
-    for (iter = bytearray.begin(); iter != bytearray.end(); iter++) {
-        bytestr += strconcat(*iter, *(iter + 1));
-    }
+	for (iter = bytearray.begin(); iter != bytearray.end(); iter++) {
+		bytestr += strconcat(*iter, *(iter + 1));
+	}
 
-    return bytestr;
+	return bytestr;
 }
 
 #endif // SHA256_HELPERS_H
