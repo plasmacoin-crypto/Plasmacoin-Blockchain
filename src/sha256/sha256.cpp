af65a9ffa0a2a29b3d4365036b255c7a9b751c74
Ryan Smith <rysmith2113@gmail.com>
2021-02-06 23:38:34 -0500

    Update the current test code

--- src/sha256/sha256.cpp
+++ src/sha256/sha256.cpp
@@ -14,18 +14,18 @@
 
 int main() {
 	// std::cout << bits.to_string();
-	//std::cout << sha256Hash("abc");
+	std::cout << sha256Hash("abc");
 	//auto arr = decompose(preprocess("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"));
   	//for (auto item: split(preprocess("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"))[0]) {
     //    std::cout << item << std::endl;
     //}
-    auto arr = split(preprocess("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopqa"))[0];
-    //std::cout << preprocess("abc");
+    //auto arr = split(preprocess("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopqa"))[0];
+    //auto arr = split(preprocess("abc"))[0];
 
-    std::cout << arr.size() << std::endl;
-	for (uint i = 0; i < arr.size(); i++) {
-		std::cout << i << ": " << arr[i] << std::endl;
-	}
+    // std::cout << arr.size() << std::endl;
+	// for (uint i = 0; i < arr.size(); i++) {
+	// 	std::cout << i << ": " << arr[i] << std::endl;
+	// }
 
 	return 0;
 }
