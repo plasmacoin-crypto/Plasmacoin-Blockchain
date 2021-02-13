e9551ee1a2d82012d8b2ed94800494f0af938e32
Ryan Smith <rysmith2113@gmail.com>
2021-02-13 00:32:32 -0500

    Use a SHA-256 library to hash the transaction data

--- src/blockchain.hpp
+++ src/blockchain.hpp
@@ -11,6 +11,8 @@
 #include <set>
 #include <queue>
 
+#include "SHA256.h"
+
 #include "block.hpp"
 
 using std::set;
@@ -28,7 +30,10 @@ private:
 	set<Block*> m_Chain;
 	queue<Block*> m_Unconfirmed; // Blocks waiting to be mined
 
+	SHA256 hasher;
+
 	bool Consensus(Block block); // Evaluate Proof-of-Work
+	string Hash(Transaction transaction);
 };
 
 Blockchain::Blockchain() {
@@ -58,6 +63,18 @@ int Blockchain::Mine() {
 
 bool Blockchain::Consensus(Block block) {
 	return false; // tmp
+}
+
+string Blockchain::Hash(Transaction transaction) {
+	// Use the SHA-256 library to has the transaction data
+	hasher.update(transaction.Condense());
+	uint8_t* digest = hasher.digest();
+
+	string hash = SHA256::toString(digest);
+
+	delete[] digest; // Free the digest
+
+	return hash;
 }
 
 #endif // BLOCKCHAIN_HPP
