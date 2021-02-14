15207c1b4ce7daef173a3b5bf1b85a3096f4e6a2
Ryan Smith <rysmith2113@gmail.com>
2021-02-14 15:42:43 -0500

    Group some header files

---
+++ src/cryptocpp-sha256-libs.h
@@ -0,0 +1,15 @@
+//
+// FILENAME: cryptocpp-sha256-libs.h | Plasmacoin Cryptocurrency
+// DESCRIPTION: Group the header files required to use the Crypto++ SHA-256 library
+// CREATED: 2021-01-18 @ 8:51 PM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#ifndef CRYPTO_CPP_SHA256_LIBS_H
+#define CRYPTO_CPP_SHA256_LIBS_H
+
+#include <cryptopp/filters.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/sha.h>
+
+#endif // CRYPTO_CPP_SHA256_LIBS_H
