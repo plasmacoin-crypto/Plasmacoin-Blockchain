dfe100ddb376535c985efcc90fd13dde7ea1e43c
Ryan Smith <rysmith2113@gmail.com>
2021-02-15 22:08:32 -0500

    Update the current test code

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,19 @@
 #include "blockchain.hpp"
 
 int main() {
+	Blockchain* chain = new Blockchain();
+
 	Node* node1 = new Node("Ryan", "ryan", "1234", "192.168.1.6");
 	Node* node2 = new Node("John", "john", "4567", "192.168.1.7");
 
 	Transaction transaction = node1->MakeTransaction(*node2, 1.0, "Here's some money");
 
+	Block block = Block(1, nullptr, &transaction);
+
 	std::cout << transaction.m_Content << std::endl;
+
+	std::cout << chain->Consensus(block) << std::endl;
+	std::cout << block.m_Hash << std::endl;
 
 	return 0;
 }
