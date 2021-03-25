9ebdb5d7407cf577bce952887ab5cf19c0b24ee0
Ryan Smith <rysmith2113@gmail.com>
2021-03-24 23:16:12 -0400

    Implement tab switching

--- src/qt/plasmacoin-ui/mainwindow.cpp
+++ src/qt/plasmacoin-ui/mainwindow.cpp
@@ -1,9 +1,18 @@
+// The source file for mainwindow.h
+
+#include <thread>
+
 #include "mainwindow.h"
+
+using std::thread;
 
 MainWindow::MainWindow(QWidget *parent):
 	QMainWindow(parent)
 {
 	setupUi(this);
+
+	class thread windowManager([this]() { this->UpdateWindow(); });
+	windowManager.join();
 }
 
 MainWindow::~MainWindow() {}
@@ -11,9 +20,8 @@ MainWindow::~MainWindow() {}
 void MainWindow::UpdateWindow() {
 	// Tab indicies should line up with stack widget indicies
 	int index = tabWidget->currentIndex();
-	//QWidget* tab = m_TabBar->currentWidget();
 
-	connect(m_TabBar, &QTabWidget::tabBarClicked, this, &MainWindow::DisplayPage);
+	connect(Ui::MainWindow::tabWidget, &QTabWidget::tabBarClicked, this, &MainWindow::DisplayPage);
 }
 
 void MainWindow::DisplayPage(int index) {
