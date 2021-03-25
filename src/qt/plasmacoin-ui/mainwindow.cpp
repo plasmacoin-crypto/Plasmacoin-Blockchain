5562164ce3e30043b5632a53f618a369b00fab6b
Ryan Smith <rysmith2113@gmail.com>
2021-03-24 21:51:20 -0400

    Add a way to switch tabs

--- src/qt/plasmacoin-ui/mainwindow.cpp
+++ src/qt/plasmacoin-ui/mainwindow.cpp
@@ -8,3 +8,15 @@ MainWindow::MainWindow(QWidget *parent):
 
 MainWindow::~MainWindow() {}
 
+void MainWindow::UpdateWindow() {
+	// Tab indicies should line up with stack widget indicies
+	int index = tabWidget->currentIndex();
+	//QWidget* tab = m_TabBar->currentWidget();
+
+	connect(m_TabBar, &QTabWidget::tabBarClicked, this, &MainWindow::DisplayPage);
+}
+
+void MainWindow::DisplayPage(int index) {
+	stackedWidget->setCurrentIndex(index);
+}
+
--- src/qt/plasmacoin-ui/mainwindow.h
+++ src/qt/plasmacoin-ui/mainwindow.h
@@ -12,6 +12,16 @@ class MainWindow : public QMainWindow, public Ui::MainWindow
 public:
 	MainWindow(QWidget *parent = nullptr);
 	~MainWindow();
+
+	// Cycle the QStackedWidget object to display the correct information
+	// on the correct tab
+	void UpdateWindow();
+
+private:
+	QTabWidget* m_TabBar = Ui::MainWindow::tabWidget;
+
+private slots:
+	void DisplayPage(int index); // Actually set the index of the stack widget
 };
 
 #endif // MAINWINDOW_H
