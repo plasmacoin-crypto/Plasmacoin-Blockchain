4c1b174a80ec865a87ee5d3eb684533f0b53d8a7
Ryan Smith <rysmith2113@gmail.com>
2021-04-01 14:40:32 -0400

    Always launch the app with QStackedWidget page 1 showing

--- src/qt/plasmacoin-ui/mainwindow.cpp
+++ src/qt/plasmacoin-ui/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent):
 	QMainWindow(parent)
 {
 	setupUi(this);
+	DisplayPage(0); // Reset the QStackedWidget to page 1 (index 0)
 
 	// Allow tab switching
 	connect(Ui::MainWindow::tabWidget, &QTabWidget::tabBarClicked, this, &MainWindow::DisplayPage);
