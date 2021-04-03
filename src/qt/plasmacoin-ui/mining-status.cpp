34f0cc9c860307e76c0011e33d2fec6df6b513fd
Ryan Smith <rysmith2113@gmail.com>
2021-04-02 23:41:26 -0400

    Add certain graphics when a block is being mined

---
+++ src/qt/plasmacoin-ui/mining-status.cpp
@@ -0,0 +1,84 @@
+//
+// FILENAME: mining-status.cpp | Plasmacoin Cryptocurrency
+// DESCRIPTION: The source file for mining-status.h
+// CREATED: 2021-04-02 @ 4:22 PM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#include "mining-status.h"
+
+Status::Status(QTextBrowser*& qtbrowser1, QTextBrowser*& qtbrowser2, QTextBrowser*& qtbrowser3):
+	m_Heading(qtbrowser1),
+	m_NonceView(qtbrowser2),
+	m_ElapsedTime(qtbrowser3)
+{}
+
+// Render the mining status visuals on the screen
+void Status::LoadVisuals() {
+	// Main display
+	m_Heading->setObjectName(QString::fromUtf8("heading"));
+	m_Heading->setGeometry(QRect(380, 30, 391, 51));
+
+	// A readout of the current nonce value
+	m_NonceView->setObjectName(QString::fromUtf8("nonceView"));
+	m_NonceView->setGeometry(QRect(380, 100, 191, 51));
+
+	// A readout of the elapsed mining time
+	m_ElapsedTime->setObjectName(QString::fromUtf8("elapsedTime"));
+	m_ElapsedTime->setGeometry(QRect(580, 100, 191, 51));
+}
+
+// Modify the heading
+void Status::SetHeading(string text) {
+	m_Heading->setHtml(
+		QString(
+			string(
+				"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">"
+				"<html>"
+					"<head>"
+						"<meta name=\"qrichtext\" content=\"1\" />"
+						"<style type=\"text/css\">"
+							"p, li {"
+								"white-space: pre-wrap;"
+							"}"
+						"</style>"
+					"</head>"
+					"<body style=\"font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">"
+						"<p align=\"center\" style=\"margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"
+							"Transaction Hash: " + text +
+						"</p>"
+					"</body>"
+				"</html>"
+			).c_str()
+		)
+	);
+}
+
+// Display the current nonce value
+void Status::SetNonce(int nonce) {
+	m_NonceView->setHtml(
+		QString(
+			string(
+				"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">"
+				"<html>"
+					"<head>"
+						"<meta name=\"qrichtext\" content=\"1\" />"
+						"<style type=\"text/css\">"
+							"p, li {"
+								"white-space: pre-wrap;"
+							"}"
+						"</style>"
+					"</head>"
+					"<body style=\"font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">"
+						"<p align=\"center\" style=\"margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"
+							"Current Nonce: " + std::to_string(nonce) +
+						"</p>"
+					"</body>"
+				"</html>"
+			).c_str()
+		)
+	);
+}
+
+// Set the current elapsed mining time
+void Status::SetTime(std::chrono::duration<float> elaspedTime) {}
---
+++ src/qt/plasmacoin-ui/mining-status.h
@@ -0,0 +1,29 @@
+//
+// FILENAME: mining-status.h | Plasmacoin Cryptocurrency
+// DESCRIPTION: Display some info boxes while a block is being mined
+// CREATED: 2021-04-02 @ 4:22 PM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#include <QString>
+#include <QTextBrowser>
+#include <QRect>
+
+#include <string>
+#include <chrono>
+
+using std::string;
+
+class Status {
+public:
+	Status(QTextBrowser*& qtbrowser1, QTextBrowser*& qtbrowser2, QTextBrowser*& qtbrowser3);
+
+	void LoadVisuals();
+
+	// Functions to modify the browser text
+	void SetHeading(string text), SetNonce(int nonce),
+		 SetTime(std::chrono::duration<float> elaspedTime);
+
+private:
+	QTextBrowser *&m_Heading, *&m_NonceView, *&m_ElapsedTime;
+};
