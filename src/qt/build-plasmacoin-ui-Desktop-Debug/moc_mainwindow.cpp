f329318dfc016a43df889823148e27b4bfca1cd2
Ryan Smith <rysmith2113@gmail.com>
2021-04-29 22:50:35 -0400

    Update the moc

--- src/qt/build-plasmacoin-ui-Desktop-Debug/moc_mainwindow.cpp
+++ src/qt/build-plasmacoin-ui-Desktop-Debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[6];
-    char stringdata0[55];
+    QByteArrayData data[4];
+    char stringdata0[30];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,13 +35,10 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 11), // "DisplayPage"
 QT_MOC_LITERAL(2, 23, 0), // ""
-QT_MOC_LITERAL(3, 24, 5), // "index"
-QT_MOC_LITERAL(4, 30, 11), // "StartMining"
-QT_MOC_LITERAL(5, 42, 12) // "Transaction*"
+QT_MOC_LITERAL(3, 24, 5) // "index"
 
     },
-    "MainWindow\0DisplayPage\0\0index\0StartMining\0"
-    "Transaction*"
+    "MainWindow\0DisplayPage\0\0index"
 };
 #undef QT_MOC_LITERAL
 
@@ -51,7 +48,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -59,12 +56,10 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x08 /* Private */,
-       4,    0,   27,    2, 0x08 /* Private */,
+       1,    1,   19,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, QMetaType::Int,    3,
-    0x80000000 | 5,
 
        0        // eod
 };
@@ -76,8 +71,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->DisplayPage((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 1: { Transaction* _r = _t->StartMining();
-            if (_a[0]) *reinterpret_cast< Transaction**>(_a[0]) = std::move(_r); }  break;
         default: ;
         }
     }
@@ -114,13 +107,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 1)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 1;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 1)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 1;
     }
     return _id;
 }
