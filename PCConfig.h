fc93971c30bd517b7da1c0cb018377cdf18dd0fd
Ryan Smith <rysmith2113@gmail.com>
2021-05-17 22:57:58 -0400

    Add PCConfig.h
    Generated by CMake

---
+++ PCConfig.h
@@ -0,0 +1,10 @@
+//
+// FILENAME: PCConfig.h.in
+// DESCRIPTION: the configured options and settings for the project
+// CREATED: 2021-05-14 @ 10:37 AM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#define Plasmacoin_Crypto_VERSION_MAJOR   0
+#define Plasmacoin_Crypto_VERSION_MINOR   1
+#define Plasmacoin_Crypto_VERSION_PATCH   0
