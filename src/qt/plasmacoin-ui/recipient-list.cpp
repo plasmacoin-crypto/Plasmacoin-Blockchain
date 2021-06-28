8fe4fbfeecdab0724ab29c147aa32b075f72624c
Ryan Smith <rysmith2113@gmail.com>
2021-06-27 21:11:50 -0400

    Add a new class

---
+++ src/qt/plasmacoin-ui/recipient-list.cpp
@@ -0,0 +1,6 @@
+//
+// FILENAME: recipient-list.cpp | Plasmacoin Cryptocurrency
+// DESCRIPTION: The source file for recipient-list.h
+// CREATED: 2021-06-03 @ 2:39 PM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
---
+++ src/qt/plasmacoin-ui/recipient-list.h
@@ -0,0 +1,15 @@
+//
+// FILENAME: recipient-list.h | Plasmacoin Cryptocurrency
+// DESCRIPTION: An item that holds a list of Plasmacoin transaction recipients
+// CREATED: 2021-06-03 @ 2:39 PM
+// COPYRIGHT: Copyright (c) 2021 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#ifndef RECIPIENT_LIST_H
+#define RECIPIENT_LIST_H
+
+class RecipientList {
+public:
+};
+
+#endif // RECIPIENT_LIST_H
