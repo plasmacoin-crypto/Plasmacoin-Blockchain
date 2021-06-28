9111c14003c3536b517d6abaa84a6b0267160265
Ryan Smith <rysmith2113@gmail.com>
2021-06-28 16:18:58 -0400

    Change a title

--- src/qt/plasmacoin-ui/mining-status.cpp
+++ src/qt/plasmacoin-ui/mining-status.cpp
@@ -45,7 +45,7 @@ void Status::SetHeading(string text) {
 					"</head>"
 					"<body style=\"font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">"
 						"<p align=\"center\" style=\"margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"
-							"Transaction Hash: " + text +
+							"Block Hash: " + text +
 						"</p>"
 					"</body>"
 				"</html>"
