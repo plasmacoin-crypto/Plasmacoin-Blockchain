1596ce7cc47ef9df0f4ac8f59fdbcc1decbfa7a7
Ryan Smith <rysmith2113@gmail.com>
2021-06-28 15:58:19 -0400

    Call the mining code from the main function

--- src/qt/plasmacoin-ui/main.cpp
+++ src/qt/plasmacoin-ui/main.cpp
@@ -1,7 +1,18 @@
 
 #include "mainwindow.h"
 
+#include "ui_mainwindow.h"
+
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QPushButton>
+
+void minePage(MainWindow& window) {
+	// Mine a block when the correct button is clicked
+	window.connect(window.btn_mine, &QPushButton::released, &window, [&window]() {
+		window.status.SetHeading("Transaction");
+		window->mine.wait();
+	});
+}
 
 int main(int argc, char *argv[]) {
 	QApplication app(argc, argv);
@@ -9,7 +20,7 @@ int main(int argc, char *argv[]) {
 	MainWindow window;
 	window.show();
 
-	window.status.SetHeading("Hello");
+	minePage(window);
 
 	return app.exec();
 }
