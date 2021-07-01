d6da9250881d6810dd20a91017583d75cbc32007
Ryan Smith <rysmith2113@gmail.com>
2021-07-01 14:44:42 -0400

    Implement Status::SetTime

--- src/qt/plasmacoin-ui/mining-status.cpp
+++ src/qt/plasmacoin-ui/mining-status.cpp
@@ -120,4 +120,27 @@ void Status::SetNonce(int nonce) {
 }
 
 // Set the current elapsed mining time
-void Status::SetTime(std::chrono::duration<float> elaspedTime) {}
+void Status::SetTime(std::chrono::seconds elapsedTime) {
+	m_ElapsedTime->setHtml(
+		QString(
+			string(
+				"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">"
+				"<html>"
+					"<head>"
+						"<meta name=\"qrichtext\" content=\"1\" />"
+						"<style type=\"text/css\">"
+							"p, li {"
+								"white-space: pre-wrap;"
+							"}"
+						"</style>"
+					"</head>"
+					"<body style=\"font-family:\'Noto Sans\'; font-size:10pt; font-weight:400; font-style:normal;\">"
+						"<p align=\"center\" style=\"margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">"
+							"Elapsed Time: " + std::to_string(elapsedTime.count()) +
+						"</p>"
+					"</body>"
+				"</html>"
+			).c_str()
+		)
+	);
+}
