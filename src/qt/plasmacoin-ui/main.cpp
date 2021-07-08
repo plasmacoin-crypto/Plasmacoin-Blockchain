1d9d53b3af59a12a6c13885ab1bf4ce039310b58
Ryan Smith <rysmith2113@gmail.com>
2021-07-08 03:29:36 -0400

    Modify how transactions are added
    * You can no longer use the plus or minus buttons unless you've selected something
    * You can no longer add duplicate items

--- src/qt/plasmacoin-ui/main.cpp
+++ src/qt/plasmacoin-ui/main.cpp
@@ -16,25 +16,37 @@ void minePage(MainWindow& window) {
 	});
 }
 
+// Allow the user to add transactions to their block
 void addToBlock(MainWindow& window) {
-	// Allow adding and removal of transactions from the miner's block
 	window.connect(window.plusSign, &QToolButton::released, &window, [&window]() {
+		auto selected = window.transactionList->selectedItems();
 		int row = window.transactionList->currentRow();
-		QListWidgetItem* item = window.transactionList->item(row); // get the item at the currently selected row
 
-		window.m_BlockContents.push_back(item); // Record the selected item
-		window.blockTransactionList->addItem(item->text());
+		// A user will only be able to add a transaction if:
+		//
+		// 1) They have selected a transaction from the list on the right (no blank selections)
+		// 2) They have not added that transaction previously (no adding duplicate elements)
+		//
+		if ((selected.size() != 0) && (window.blockTransactionList->findItems(selected.front()->text(), Qt::MatchFixedString).size() == 0)) {
+			QListWidgetItem* item = window.transactionList->item(row); // Get the item at the currently selected row
+
+			window.m_BlockContents.push_back(item); // Record the selected item
+			window.blockTransactionList->addItem(item->text());
+		}
 	});
 }
 
+// Allow the user to remove transactions from their block
 void removeFromBlock(MainWindow& window) {
-	// Allow adding and removal of transactions from the miner's block
 	window.connect(window.minusSign, &QToolButton::released, &window, [&window]() {
-		int row = window.blockTransactionList->currentRow();
-		QListWidgetItem* item = window.blockTransactionList->item(row); // get the item at the currently selected row
+		// To remove a transaction, the only requirement is that it's selected
+		if (window.blockTransactionList->selectedItems().size() != 0) {
+			int row = window.blockTransactionList->currentRow();
+			QListWidgetItem* item = window.blockTransactionList->item(row); // get the item at the currently selected row
 
-		window.m_BlockContents.erase(window.m_BlockContents.begin() + row); // Remove the selected item
-		window.blockTransactionList->takeItem(row);
+			window.m_BlockContents.erase(window.m_BlockContents.begin() + row); // Remove the selected item
+			window.blockTransactionList->takeItem(row);
+		}
 	});
 }
 
