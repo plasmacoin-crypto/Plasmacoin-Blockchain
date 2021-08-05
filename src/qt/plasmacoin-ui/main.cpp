4c46b4fdc6c4e27c2f46a7b5b821a691e3087bcf
Ryan Smith <rysmith2113@gmail.com>
2021-08-05 17:20:43 -0400

    Connect some buttons and error handling messages to the account page

--- src/qt/plasmacoin-ui/main.cpp
+++ src/qt/plasmacoin-ui/main.cpp
@@ -6,13 +6,64 @@
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "firebase-auth.h"
 
+// Allow the user to mine their block
 void minePage(MainWindow& window) {
-	window.status.SetHeading("Mining block");
+	window.m_Status.SetHeading("Mining block");
 
 	// Mine a block when the correct button is clicked
 	window.connect(window.btn_mine, &QPushButton::released, &window, [&window]() {
 		window.mine.wait();
+	});
+}
+
+// Allow the user to type information in forms (QLineEdits) and be authenticated to
+// the app through the Firebase REST API
+void accountPages(MainWindow& window) {
+	// Take a user from the sign in screen to the sign up screen
+	window.connect(window.btn_create, &QPushButton::released, &window, [&window]() {
+		window.m_AccPgs->DisplayPage(1);
+	});
+
+	// Sign a user into their account
+	window.connect(window.btn_signIn, &QPushButton::released, &window, [&window]() {
+		QString email, username, password;
+		std::tie(email, username, password) = window.m_AccPgs->ReadText(); // Get the user's entries
+
+		window.m_Authenticator->SignIn(email, password);
+	});
+
+	window.connect(window.btn_signUp, &QPushButton::released, &window, [&window]() {
+		QString email, username, password;
+		std::tie(email, username, password) = window.m_AccPgs->ReadText(); // Get the user's entries
+
+		window.m_Authenticator->SignUp(email, username, password);
+	});
+
+	// Take the user to the logged in view if Firebase successfully logged them in
+	window.connect(window.m_Authenticator, &Auth::UserSignedIn, &window, [&window]() {
+		if (!window.m_Authenticator->SearchFor("error")) {
+			window.m_AccPgs->DisplayPage(2);
+		}
+	});
+
+	// Display a warning when a user tries to create an account with an email that's in use
+	window.connect(window.m_Authenticator, &Auth::EmailExists, &window, [&window]() {
+		window.m_AccPgs->m_EmailWarning->setText("Email in use");
+		window.m_AccPgs->m_EmailWarning->setVisible(true);
+	});
+
+	// Display a warning when a user tries to create an account with an invalid email
+	window.connect(window.m_Authenticator, &Auth::InvalidEmail, &window, [&window]() {
+		window.m_AccPgs->m_EmailWarning->setText("Invalid email");
+		window.m_AccPgs->m_EmailWarning->setVisible(true);
+	});
+
+	// Display a warning when a user tries to create an account with an email that's in use
+	window.connect(window.m_Authenticator, &Auth::UsernameTaken, &window, [&window]() {
+		window.m_AccPgs->m_EmailWarning->setText("Username already in use");
+		window.m_AccPgs->m_EmailWarning->setVisible(true);
 	});
 }
 
@@ -39,7 +90,8 @@ void addToBlock(MainWindow& window) {
 // Allow the user to remove transactions from their block
 void removeFromBlock(MainWindow& window) {
 	window.connect(window.minusSign, &QToolButton::released, &window, [&window]() {
-		// To remove a transaction, the only requirement is that it's selected
+		// To remove a transaction, the only requirement is that it's been selected on
+		// the list of transactions in user's block
 		if (window.blockTransactionList->selectedItems().size() != 0) {
 			int row = window.blockTransactionList->currentRow();
 			QListWidgetItem* item = window.blockTransactionList->item(row); // get the item at the currently selected row
@@ -50,15 +102,18 @@ void removeFromBlock(MainWindow& window) {
 	});
 }
 
-int main(int argc, char *argv[]) {
+// Run the application
+int main(int argc, char* argv[]) {
 	QApplication app(argc, argv);
 
 	MainWindow window;
 	window.show();
 
+	// Connect everything
 	minePage(window);
 	addToBlock(window);
 	removeFromBlock(window);
+	accountPages(window);
 
 	return app.exec();
 }
