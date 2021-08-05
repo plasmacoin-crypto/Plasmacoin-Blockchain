a97b4527521dca710e3de8d8507e730d4c649c6c
Ryan Smith <rysmith2113@gmail.com>
2021-08-05 17:20:57 -0400

    Update the moc

--- src/qt/build-plasmacoin-ui-Desktop-Debug/moc_firebase_auth.cpp
+++ src/qt/build-plasmacoin-ui-Desktop-Debug/moc_firebase_auth.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Auth_t {
-    QByteArrayData data[6];
-    char stringdata0[65];
+    QByteArrayData data[9];
+    char stringdata0[104];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,11 +36,15 @@ QT_MOC_LITERAL(0, 0, 4), // "Auth"
 QT_MOC_LITERAL(1, 5, 12), // "UserSignedIn"
 QT_MOC_LITERAL(2, 18, 0), // ""
 QT_MOC_LITERAL(3, 19, 14), // "RequestedToken"
-QT_MOC_LITERAL(4, 34, 17), // "NetworkReplyReady"
-QT_MOC_LITERAL(5, 52, 12) // "RequestToken"
+QT_MOC_LITERAL(4, 34, 11), // "EmailExists"
+QT_MOC_LITERAL(5, 46, 12), // "InvalidEmail"
+QT_MOC_LITERAL(6, 59, 13), // "UsernameTaken"
+QT_MOC_LITERAL(7, 73, 17), // "NetworkReplyReady"
+QT_MOC_LITERAL(8, 91, 12) // "RequestToken"
 
     },
     "Auth\0UserSignedIn\0\0RequestedToken\0"
+    "EmailExists\0InvalidEmail\0UsernameTaken\0"
     "NetworkReplyReady\0RequestToken"
 };
 #undef QT_MOC_LITERAL
@@ -51,22 +55,28 @@ static const uint qt_meta_data_Auth[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       2,       // signalCount
+       5,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x06 /* Public */,
-       3,    0,   35,    2, 0x06 /* Public */,
+       1,    0,   49,    2, 0x06 /* Public */,
+       3,    0,   50,    2, 0x06 /* Public */,
+       4,    0,   51,    2, 0x06 /* Public */,
+       5,    0,   52,    2, 0x06 /* Public */,
+       6,    0,   53,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       4,    0,   36,    2, 0x0a /* Public */,
-       5,    0,   37,    2, 0x0a /* Public */,
+       7,    0,   54,    2, 0x0a /* Public */,
+       8,    0,   55,    2, 0x0a /* Public */,
 
  // signals: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -85,8 +95,11 @@ void Auth::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         switch (_id) {
         case 0: _t->UserSignedIn(); break;
         case 1: _t->RequestedToken(); break;
-        case 2: _t->NetworkReplyReady(); break;
-        case 3: _t->RequestToken(); break;
+        case 2: _t->EmailExists(); break;
+        case 3: _t->InvalidEmail(); break;
+        case 4: _t->UsernameTaken(); break;
+        case 5: _t->NetworkReplyReady(); break;
+        case 6: _t->RequestToken(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -102,6 +115,27 @@ void Auth::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
             using _t = void (Auth::*)();
             if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Auth::RequestedToken)) {
                 *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (Auth::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Auth::EmailExists)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (Auth::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Auth::InvalidEmail)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (Auth::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Auth::UsernameTaken)) {
+                *result = 4;
                 return;
             }
         }
@@ -140,13 +174,13 @@ int Auth::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 7;
     }
     return _id;
 }
@@ -161,6 +195,24 @@ void Auth::UserSignedIn()
 void Auth::RequestedToken()
 {
     QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
+}
+
+// SIGNAL 2
+void Auth::EmailExists()
+{
+    QMetaObject::activate(this, &staticMetaObject, 2, nullptr);
+}
+
+// SIGNAL 3
+void Auth::InvalidEmail()
+{
+    QMetaObject::activate(this, &staticMetaObject, 3, nullptr);
+}
+
+// SIGNAL 4
+void Auth::UsernameTaken()
+{
+    QMetaObject::activate(this, &staticMetaObject, 4, nullptr);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
