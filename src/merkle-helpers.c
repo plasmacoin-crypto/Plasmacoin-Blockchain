e0a1bdd49a55eecaa1adce30b37a83deaff871a8
Ryan Smith <rysmith2113@gmail.com>
2021-08-25 23:17:22 -0400

    Update a comment

--- src/merkle-helpers.c
+++ src/merkle-helpers.c
@@ -13,7 +13,7 @@ int mh_height(int leaves) {
 	return (int)(ceil(log2(leaves)) + 1);
 }
 
-// Get the number of nodes in the Merkle Tree ree based on the
+// Get the number of nodes in the Merkle Tree based on the
 // tree's height.
 int mh_nodes(int height) {
 	return (int)(pow(2, height) - 1);
