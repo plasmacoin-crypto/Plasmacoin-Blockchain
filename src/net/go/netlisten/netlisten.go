0445d60bfd65567ee939c3af09b4ac32a79c77f1
Ryan Smith <rysmith2113@gmail.com>
2021-10-11 16:52:59 -0400

    Change how connections are accepted

--- src/net/go/netlisten/netlisten.go
+++ src/net/go/netlisten/netlisten.go
@@ -8,26 +8,23 @@ import "C"
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net"
 	"strconv"
+	"sync"
 )
 
 // Read the data from a connection
 func handleConnection(conn net.Conn) {
 	buf, err := ioutil.ReadAll(conn)
+	check(err, 20)
 
-	if err != nil {
-		log.Fatal("Error while reading.")
-	}
-
-	fmt.Println(string(buf))
+	fmt.Println(buf)
 }
 
 // Check for errors
 func check(err error, line int) {
 	if err != nil {
-		log.Fatal(err.Error() + " on line " + strconv.Itoa(line))
+		var _ error = fmt.Errorf(err.Error(), " on line ", strconv.Itoa(line))
 	}
 }
 
@@ -44,15 +41,21 @@ func listen(protocol, host, port string) (net.Listener, error) {
 
 // Accept a TCP connection from a client
 func accept(listener net.Listener) {
-	for {
-		conn, err := listener.Accept()
-		check(err, 49)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
-		go handleConnection(conn)
-	}
+	conn, err := listener.Accept()
+	check(err, 53)
+
+	go func(wg *sync.WaitGroup) {
+		handleConnection(conn)
+		wg.Done()
+	}(&wg)
+
+	wg.Wait()
 }
 
-// Recieve and accept TCP/UDP connections
+// Listen for and accept TCP/UDP connections
 //export Receive
 func Receive(protocol, host, port C.cchar_t) {
 	// Convert the C strings to Go strings
@@ -62,10 +65,32 @@ func Receive(protocol, host, port C.cchar_t) {
 		goPort     = C.GoString(port)
 	)
 
-	listener, err := listen(goProtocol, goHost, goPort)
-	check(err, 66)
+	fmt.Println(goHost)
 
-	accept(listener)
+	// Listen for a connection
+	listener, err := listen(goProtocol, goHost, goPort)
+	check(err, 77)
+
+	defer listener.Close()
+
+	for {
+		// Wait for a connection.
+		conn, err := listener.Accept()
+		check(err, 84)
+
+		// Handle the connection
+		go handleConnection(conn)
+	}
 }
 
-func main() {}
+func main() {
+	Receive(C.CString("tcp"), C.CString("192.168.1.6"), C.CString("8080"))
+
+	// l, _ := listen("tcp", "192.168.1.6", "8080")
+	// accept(l)
+	// Receive(C.CString("tcp"), C.CString("192.168.1.6"), C.CString("8080"))
+	// Dial(C.CString("tcp"), C.CString("192.168.1.6"), C.CString("8080"), C.CString("Hello, World!"))
+	// accept(l)
+}
+
+//func main() {}
