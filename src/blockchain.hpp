b57238cb543514bd4fbb88de7788fbcccb64cdc9
Ryan Smith <rysmith2113@gmail.com>
2021-10-11 16:47:28 -0400

    Include Go code

--- src/blockchain.hpp
+++ src/blockchain.hpp
@@ -10,21 +10,32 @@
 
 #include <vector>
 #include <iostream>
+#include <thread>
 
 #include <QDebug>
 
 // This header file groups three Crypto++ headers to save space
-// and also provide them with a descriptive overall name.
+// and also provide them with a descriptive, overall name.
 #include "cryptopp-sha256-libs.h"
 
 #include "block.hpp"
 #include "merkle-helpers.h"
+#include "dat-fs.hpp"
+
+// Include Go code
+namespace go {
+	#include "compression.h"
+	#include "netutils.h"
+	#include "netlisten.h"
+	#include "netdial.h"
+}
 
 using std::vector;
 
 class Blockchain {
 public:
 	Blockchain();
+	~Blockchain();
 
 	// Both will return 0 on success
 	int Add(Block* block); // Add a block with a confirmed transaction to the blockchain
@@ -34,15 +45,17 @@ public:
 	Block* GetLatest() const;
 	size_t Size() const;
 
-	bool Mine(Block& newBlock);
-
 private:
 	const int DIFFICULTY = 5;
 
 	vector<Block*> m_Chain;
 	vector<Transaction*> m_Unconfirmed; // Blocks waiting to be mined (the ledger)
 
+	void Save(Block* block) const;
+	void Compress();
+
 public:
+	bool Mine(Block& newBlock);
 	bool Consensus(Block& block); // Evaluate Proof-of-Work
 	bool Validate();
 
