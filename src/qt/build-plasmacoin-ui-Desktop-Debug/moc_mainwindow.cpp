82fbb6dbb1f32ed8e5d48d077df5c3a193d0a337
Ryan Smith <rysmith2113@gmail.com>
2021-11-22 13:42:10 -0500

    Update the moc

--- src/qt/build-plasmacoin-ui-Desktop-Debug/moc_mainwindow.cpp
+++ src/qt/build-plasmacoin-ui-Desktop-Debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[5];
-    char stringdata0[41];
+    QByteArrayData data[6];
+    char stringdata0[58];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -33,14 +33,15 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 10), // "DoneMining"
-QT_MOC_LITERAL(2, 22, 0), // ""
-QT_MOC_LITERAL(3, 23, 11), // "DisplayPage"
-QT_MOC_LITERAL(4, 35, 5) // "index"
+QT_MOC_LITERAL(1, 11, 13), // "MiningSuccess"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 13), // "MiningFailure"
+QT_MOC_LITERAL(4, 40, 11), // "DisplayPage"
+QT_MOC_LITERAL(5, 52, 5) // "index"
 
     },
-    "MainWindow\0DoneMining\0\0DisplayPage\0"
-    "index"
+    "MainWindow\0MiningSuccess\0\0MiningFailure\0"
+    "DisplayPage\0index"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,24 +51,26 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       1,       // signalCount
+       2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x06 /* Public */,
+       1,    0,   29,    2, 0x06 /* Public */,
+       3,    0,   30,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    1,   25,    2, 0x08 /* Private */,
+       4,    1,   31,    2, 0x08 /* Private */,
 
  // signals: parameters
     QMetaType::Void,
+    QMetaType::Void,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, QMetaType::Int,    5,
 
        0        // eod
 };
@@ -78,16 +81,24 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWindow *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->DoneMining(); break;
-        case 1: _t->DisplayPage((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 0: _t->MiningSuccess(); break;
+        case 1: _t->MiningFailure(); break;
+        case 2: _t->DisplayPage((*reinterpret_cast< int(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
         int *result = reinterpret_cast<int *>(_a[0]);
         {
             using _t = void (MainWindow::*)();
-            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::DoneMining)) {
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::MiningSuccess)) {
                 *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::MiningFailure)) {
+                *result = 1;
                 return;
             }
         }
@@ -125,21 +136,27 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
 
 // SIGNAL 0
-void MainWindow::DoneMining()
+void MainWindow::MiningSuccess()
 {
     QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
+}
+
+// SIGNAL 1
+void MainWindow::MiningFailure()
+{
+    QMetaObject::activate(this, &staticMetaObject, 1, nullptr);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
