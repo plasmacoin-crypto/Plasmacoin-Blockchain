9419d9e975ac9fcf0ea8d3ae46d18ba4e2cf9545
Ryan <rysmith2113@gmail.com>
2022-02-26 14:52:05 -0500

    Move the transaction signature to its own file

---
+++ blockchainconstructs/signature.go
@@ -0,0 +1,14 @@
+//
+// FILENAME: signature.go | Plasmacoin Cryptocurrency
+// DESCRIPTION: Express a digital signature natively in Go
+// CREATED: 2021-02-13 @ 12:47 PM
+// COPYRIGHT: Copyright (c) 2022 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+package blockchainconstructs
+
+type Signature struct {
+	Signature string `json:"signature"`
+	PublicKey []byte `json:"publicKey"`
+	Length    int    `json:"length"`
+}
