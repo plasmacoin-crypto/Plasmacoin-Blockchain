a6e11e64edb77203924bd48b4c2a260b963c4eae
Ryan <rysmith2113@gmail.com>
2022-02-26 14:58:37 -0500

    Add the new transaction fields

--- blockchainconstructs/transaction.go
+++ blockchainconstructs/transaction.go
@@ -9,16 +9,12 @@ package blockchainconstructs
 
 import "strconv"
 
-type Signature struct {
-	Signature string `json:"signature"`
-	PublicKey []byte `json:"publicKey"`
-	Length    int    `json:"length"`
-}
-
 type Transaction struct {
 	PacketType    int       `json:"type"`
 	SenderAddr    string    `json:"senderAddr"`
 	RecipientAddr string    `json:"recipientAddr"`
+	CreationTime  string    `json:"created"`
+	SigningTime   string    `json:"signed"`
 	Amount        float32   `json:"amount"`
 	Fee           float32   `json:"fee"`
 	Content       string    `json:"content"`
@@ -33,28 +29,30 @@ func MakeTransaction(data []string) *Transaction {
 	// When this function receives the data, all data points will be strings and
 	// are guaranteed be in the following order:
 	//
-	// 1. The packet type (as an integer)		(data[0])
-	// 2. Address of sender 					(data[1])
-	// 3. Address of recipient 					(data[2])
-	// 4. Transaction amount 					(data[3])
-	// 5. Transaction fee 						(data[4])
-	// 6. The transaction content 				(data[5])
-	// 7. The transaction signature 			(data[6])
-	// 8. The sender's public key 				(data[7])
-	// 9. The length of the signature 			(data[8])
-	// 10. The SHA-256 hash of the transaction 	(data[9])
+	// 1.  The packet type (as an integer)		(data[0])
+	// 2.  Address of sender 					(data[1])
+	// 3.  Address of recipient 				(data[2])
+	// 4.  The time the transaction was created (data[3])
+	// 5.  The time the transaction was signed 	(data[4])
+	// 6.  Transaction amount 					(data[5])
+	// 7.  Transaction fee 						(data[6])
+	// 8.  The transaction content 				(data[7])
+	// 9.  The transaction signature 			(data[8])
+	// 10. The sender's public key 				(data[9])
+	// 11. The length of the signature 			(data[10])
+	// 12. The SHA-256 hash of the transaction 	(data[11])
 	//
 
 	// Convert some stringified numeric values back to numeric values
 	packetType, _ := strconv.ParseInt(data[0], 10, 32)
-	amount, _ := strconv.ParseFloat(data[3], 32)
-	fee, _ := strconv.ParseFloat(data[4], 32)
-	siglen, _ := strconv.ParseInt(data[8], 10, 32)
+	amount, _ := strconv.ParseFloat(data[5], 32)
+	fee, _ := strconv.ParseFloat(data[6], 32)
+	siglen, _ := strconv.ParseInt(data[10], 10, 32)
 
 	// Construct the signature field
 	signature := Signature{
-		Signature: data[6],
-		PublicKey: []byte(data[7]),
+		Signature: data[8],
+		PublicKey: []byte(data[9]),
 		Length:    int(siglen),
 	}
 
@@ -63,10 +61,12 @@ func MakeTransaction(data []string) *Transaction {
 		PacketType:    int(packetType),
 		SenderAddr:    data[1],
 		RecipientAddr: data[2],
+		CreationTime:  data[3],
+		SigningTime:   data[4],
 		Amount:        float32(amount),
 		Fee:           float32(fee),
-		Content:       data[5],
+		Content:       data[7],
 		Signature:     signature,
-		Hash:          data[9],
+		Hash:          data[11],
 	}
 }
