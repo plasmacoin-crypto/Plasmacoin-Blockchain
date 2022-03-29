ae5e2817155120dd4f54a315d6f0ef465bb59de4
Ryan <rysmith2113@gmail.com>
2022-03-29 18:56:24 -0400

    Add new conversion and ecoding/decoding functions

--- src/utility.cpp
+++ src/utility.cpp
@@ -7,12 +7,13 @@
 
 #include "utility.hpp"
 
-std::string utility::getUTCTime() {
+string utility::getUTCTime() {
 	#if __cplusplus >= 202002L
 		// Since C++20, we can get the UTC time through std::chrono::utc_clock
-		std::cout << "Here" << std::endl;
+
 		auto now = std::chrono::utc_clock::now();
 		std::time_t timeNow = std::chrono::system_clock::to_time_t(now);
+
 		return std::ctime(&timeNow);
 	#else
 		// Get the UTC time using the C method
@@ -23,8 +24,77 @@ std::string utility::getUTCTime() {
 		char buf[50]; // This should be long enough for the current date and future ones as well
 		std::strftime(buf, sizeof(buf), "%Y-%m-%d %X", timeNow);
 
-		std::string strbuf = buf;
+		string strbuf = buf;
 		strbuf.shrink_to_fit();
 		return strbuf;
 	#endif
+}
+
+// Encode a raw RSA key in Base64
+string utility::pubKeyToBase64(const CryptoPP::RSAFunction& rsaKey) {
+	CryptoPP::ByteQueue queue;
+	CryptoPP::Base64Encoder encoder;
+
+	rsaKey.Save(queue);
+	rsafs::saveBase64(rsafs::TMP_PATH.c_str(), queue);
+
+	return rsafs::readBase64(rsafs::TMP_PATH.c_str(), queue);
+}
+
+// Decode a Base64 RSA key to a raw RSA key
+CryptoPP::RSAFunction utility::pubKeyFromBase64(const string& base64key) {
+	CryptoPP::Base64Decoder decoder;
+	CryptoPP::ByteQueue queue;
+	decoder.Put((const CryptoPP::byte*)base64key.data(), base64key.size());
+	decoder.MessageEnd();
+
+	rsafs::saveToFile(rsafs::TMP_PATH.c_str(), decoder);
+	CryptoPP::RSAFunction rsaKey;
+
+	try {
+		CryptoPP::FileSource file(rsafs::TMP_PATH.c_str(), true);
+		rsaKey.BERDecode(file); // Clean up the temporary file
+
+		fs::remove(rsafs::TMP_PATH);
+		return rsaKey;
+	}
+	catch (const CryptoPP::Exception& e) {
+		std::cerr << e.what() << std::endl;
+		fs::remove(rsafs::TMP_PATH); // Clean up the temporary file
+   		exit(1);
+	}
+}
+
+// Encode an RSA signature (as a CryptoPP::SecByteBlock) in Base64
+string utility::sigToBase64(const CryptoPP::SecByteBlock& signature) {
+	string base64sig;
+	CryptoPP::Base64Encoder encoder(new CryptoPP::StringSink(base64sig));
+
+	encoder.Put(signature.begin(), signature.size());
+	encoder.MessageEnd();
+
+	return base64sig;
+}
+
+// Decode a Base64 RSA signature to a CryptoPP::SecByteBlock
+CryptoPP::SecByteBlock utility::sigFromBase64(const string& base64sig) {
+	string strSig;
+
+	CryptoPP::StringSource ss(base64sig, true,
+    	new CryptoPP::Base64Decoder(
+        	new CryptoPP::StringSink(strSig)
+    	) // Base64Decoder
+	); // StringSource
+
+	return utility::sbbFromString(strSig);
+}
+
+// Convert a CryptoPP::SecByteBlock to a std::string
+string utility::sbbToString(const CryptoPP::SecByteBlock& sbb) {
+	return string(reinterpret_cast<const char*>(&sbb[0]), sbb.size());
+}
+
+// Convert a std::string to a CryptoPP::SecByteBlock
+CryptoPP::SecByteBlock utility::sbbFromString(const string& strSbb) {
+	return CryptoPP::SecByteBlock(reinterpret_cast<const CryptoPP::byte*>(&strSbb[0]), strSbb.size());
 }
--- src/utility.hpp
+++ src/utility.hpp
@@ -8,12 +8,45 @@
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
 
+// We aren't allowed to have a using directive for `std::filesystem`, so
+// I'll create an alias for the namespace called `fs`.
+#if __cplusplus >= 201703L /* C++17 or later (recommended) */
+	#include <filesystem>
+	namespace fs = std::filesystem;
+#elif __cplusplus == 201402L /* C++14 (experimental usage) */
+	#include <experimental/filesystem>
+	namespace fs = std::experimental::filesystem; // This way, `filesystem::exists()` (`fs::exists()`) always works
+#endif
+
 #include <string>
 #include <ctime>
 #include <chrono>
 
+using std::string;
+
+#include <cryptopp/rsa.h> 	   	 // Use Crypto++'s RSA functionality
+#include <cryptopp/files.h>    	 // Use FileSink
+#include <cryptopp/queue.h>    	 // Use ByteQueue
+#include <cryptopp/base64.h>	 // Use Base64Encoder/Base64Decoder
+#include <cryptopp/filters.h> 	 // Use StringSink
+#include <cryptopp/config_int.h> // Use CryptoPP::byte
+#include <cryptopp/secblock.h> 	 // Use SecByteBlock
+
+using CryptoPP::RSA;
+
+#include "rsa-fs.hpp"
+
 namespace utility {
-	std::string getUTCTime();
+	string getUTCTime();
+
+	string pubKeyToBase64(const CryptoPP::RSAFunction& rsaKey);
+	CryptoPP::RSAFunction pubKeyFromBase64(const string& base64key);
+
+	string sigToBase64(const CryptoPP::SecByteBlock& signature);
+	CryptoPP::SecByteBlock sigFromBase64(const string& base64sig);
+
+	string sbbToString(const CryptoPP::SecByteBlock& sbb);
+	CryptoPP::SecByteBlock sbbFromString(const string& strSbb);
 }
 
 #endif // UTILITY_HPP
