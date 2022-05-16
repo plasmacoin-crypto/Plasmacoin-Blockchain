f0e1fbc8ace8f828cc2f349e16abe876ed7f9dfd
Ryan <rysmith2113@gmail.com>
2022-05-15 22:19:40 -0400

    Add a sync request

---
+++ src/sync-request.hpp
@@ -0,0 +1,15 @@
+//
+// FILENAME: sync-request.hpp | Plasmacoin Cryptocurrency
+// DESCRIPTION: Request to sync data with another node
+// CREATED: 2022-04-10 @ 10:03 PM
+// COPYRIGHT: Copyright (c) 2022 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#ifndef SYNC_REQUEST_HPP
+#define SYNC_REQUEST_HPP
+
+struct SyncRequest {
+	int m_SyncType;
+};
+
+#endif // SYNC_REQUEST_HPP
