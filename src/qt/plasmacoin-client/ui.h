a981808b8940bc268a1f99bf16d2f9f1e8245f17
Ryan <rysmith2113@gmail.com>
2022-05-21 17:17:52 -0400

    Add a way to detect what UI header to include

---
+++ src/qt/plasmacoin-client/ui.h
@@ -0,0 +1,14 @@
+//
+// FILENAME: ui.h | Plasmacoin Cryptocurrency
+// DESCRIPTION: Include the correct UI header for each operating system
+// CREATED: 2021-05-21 @ 3:06 PM
+// COPYRIGHT: Copyright (c) 2022 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#ifdef __LINUX__
+	#include "ui_linux.h"
+#elif defined(__APPLE__)
+	#include "ui_macos.h"
+#elif defined(_WIN32)
+	// Include Windows UI header
+#endif
