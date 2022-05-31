950320799b3f35b309024ada9106580eb5edc0c2
Ryan Smith <rysmith2113@gmail.com>
2022-05-31 07:02:56 -0400

    Use C++ type casts

--- src/merkle-helpers.cpp
+++ src/merkle-helpers.cpp
@@ -10,13 +10,13 @@
 // Predict the height of a Merkle Tree based on the number of
 // leaves it has.
 int mh_height(int leaves) {
-	return (int)(ceil(log2(leaves)) + 1);
+	return static_cast<int>(ceil(log2(leaves)) + 1);
 }
 
 // Get the number of nodes in the Merkle Tree based on the
 // tree's height.
 int mh_nodes(int height) {
-	return (int)(pow(2, height) - 1);
+	return static_cast<int>(pow(2, height) - 1);
 }
 
 // Calculate the number of times Merkle Tree nodes will need to
