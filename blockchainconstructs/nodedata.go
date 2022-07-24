ffa244d1e3a4a95be7ac85c3f054b6a72bbc5d3d
Ryan Smith <rysmith2113@gmail.com>
2022-07-24 19:58:40 -0400

    Send port data as part of the NodeData struct

--- blockchainconstructs/nodedata.go
+++ blockchainconstructs/nodedata.go
@@ -12,7 +12,9 @@ import "strconv"
 type NodeData struct {
 	PacketType int    `json:"type"`
 	SenderIP   string `json:"senderIP"`
+	SenderPort int    `json:"senderPort"`
 	IP         string `json:"ip"`
+	Port       int    `json:"port"`
 	Address    string `json:"address"`
 	NodeType   int    `json:"nodetype"`
 	Register   bool   `json:"register"`
@@ -27,23 +29,29 @@ func MakeNode(data []string) *NodeData {
 	//
 	// 1. The packet type (as an integer)					 (data[0])
 	// 2. The sender's IP address 							 (data[1])
-	// 2. The node's IP Address 							 (data[2])
-	// 3. The node's Plasmacoin address 					 (data[3])
-	// 3. The type of node 									 (data[4])
-	// 4. If the node should be added to the lookup database (data[5])
+	// 3. The sender's port 							 	 (data[2])
+	// 4. The node's IP Address 							 (data[3])
+	// 5. The node's port 								 	 (data[4])
+	// 6. The node's Plasmacoin address 					 (data[5])
+	// 7. The type of node 									 (data[6])
+	// 8. If the node should be added to the lookup database (data[7])
 	//
 
 	// Convert some stringified numeric values back to numeric values
 	packetType, _ := strconv.ParseInt(data[0], 10, 32)
-	nodeType, _ := strconv.ParseInt(data[4], 10, 32)
-	register, _ := strconv.ParseBool(data[5])
+	senderPort, _ := strconv.ParseInt(data[2], 10, 16)
+	port, _ := strconv.ParseInt(data[4], 10, 16)
+	nodeType, _ := strconv.ParseInt(data[6], 10, 32)
+	register, _ := strconv.ParseBool(data[7])
 
 	// Construct the node data
 	return &NodeData{
 		PacketType: int(packetType),
 		SenderIP:   data[1],
-		IP:         data[2],
-		Address:    data[3],
+		SenderPort: int(senderPort),
+		IP:         data[3],
+		Port:       int(port),
+		Address:    data[5],
 		NodeType:   int(nodeType),
 		Register:   register,
 	}
