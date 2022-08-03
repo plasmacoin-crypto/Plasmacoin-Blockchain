76b4fc4f9c6fda7cf21caf9d3f178d5dadaba0d5
Ryan Smith <rysmith2113@gmail.com>
2022-08-02 20:00:22 -0400

    Add a way to use the application in closed beta mode

---
+++ src/declare-beta-release.h
@@ -0,0 +1,15 @@
+//
+// FILENAME: declare-beta-release.h | Plasmacoin Cryptocurrency
+// DESCRIPTION: Define a macro to enable code for the beta release
+// CREATED: 2022-08-01 @ 4:23 PM
+// COPYRIGHT: Copyright (c) 2022 by Ryan Smith <rysmith2113@gmail.com>
+//
+
+#ifndef DECLARE_BETA_RELEASE_H
+#define DECLARE_BETA_RELEASE_H
+
+#ifndef BETA_RELEASE
+	#define BETA_RELEASE
+#endif
+
+#endif // DECLARE_BETA_RELEASE_H
